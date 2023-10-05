ccac63de8a280392954c894aeb8f1be67e97af50
Jason P <greg_prater@yahoo.com>
2023-10-05 18:27:31 -0400

    Initial Teleop
    Changed for initial Teleop for drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardwareMap.java
@@ -31,7 +31,7 @@ public class RobotHardwareMap {
     public HardwareMap baseHMap;
 
     public VoltageSensor controlHubBatteryVoltage;
-    public VoltageSensor expansionHubBatteryVoltage;
+    //public VoltageSensor expansionHubBatteryVoltage;
 
     public LynxModule controlHub;
     public LynxModule expansionHub;
@@ -41,67 +41,30 @@ public class RobotHardwareMap {
     public DcMotorEx frontLeftMotor;
     public DcMotorEx frontRightMotor;
 
-    public DcMotorEx elevatorMotor;
-    public TouchSensor elevatorMotorLowerLimitSwitch;
-    public TouchSensor elevatorMotorUpperLimitSwitch;
-    public ColorSensor elevatorColorSensor;
-    public TouchSensor fourBarLowerLimitSwitch;
-    public DcMotorEx fourBarMotor;
-    public AnalogInput fourBarPot;
-    public DigitalChannel leftRedLED;
-    public DigitalChannel leftGreenLED;
-    public DigitalChannel rightRedLED;
-    public DigitalChannel rightGreenLED;
-    public DigitalChannel frontLeftRedLED;
-    public DigitalChannel frontLeftGreenLED;
-    public DigitalChannel frontRightRedLED;
-    public DigitalChannel frontRightGreenLED;
+    //public ColorSensor elevatorColorSensor;
+    //public DigitalChannel leftRedLED;
+    //public DigitalChannel leftGreenLED;
+    //public DigitalChannel rightRedLED;
+    //public DigitalChannel rightGreenLED;
+    //public DigitalChannel frontLeftRedLED;
+    //public DigitalChannel frontLeftGreenLED;
+    //public DigitalChannel frontRightRedLED;
+    //public DigitalChannel frontRightGreenLED;
 
-    public DcMotor leftTowerMotor;
-    public DcMotor rightTowerMotor;
-    public DcMotor intakeMotor;
+    //public DcMotor leftTowerMotor;
+    //public DcMotor rightTowerMotor;
+    //public DcMotor intakeMotor;
 
-    public Servo clawServo;
-    public Servo wristServo;
-    public ColorSensor shirtSensor;
-    public DistanceSensor coneDistanceSensor;
     public BNO055IMU chImu;
     public BNO055IMU ehImu;
     public BNO055IMU fbImu;
 
     private final int baseResolution_x = 320;
     private final int baseResolution_y = 240;
-    WebcamName parkingCamera;
     WebcamName frontCamera;
 
     boolean controlHubBatteryVoltageEnabled = true;
     boolean expansionHubBatteryVoltageEnabled = true;
-    boolean backLeftMotorEnabled = true;
-    boolean backRightMotorEnabled = true;
-    boolean frontLeftMotorEnabled = true;
-    boolean frontRightMotorEnabled = true;
-    boolean elevatorMotorEnabled = true;
-    boolean elevatorMotorLowerLimitSwitchEnabled = true;
-    boolean elevatorMotorUpperLimitSwitchEnabled = true;
-    boolean elevatorColorSensorEnabled = false;
-    boolean fourBarLowerLimitSwitchEnabled = true;
-    boolean fourBarMotorEnabled = true;
-    boolean fourBarPotEnabled = false;
-    boolean leftRedLEDEnabled = true;
-    boolean leftGreenLEDEnabled = true;
-    boolean rightRedLEDEnabled = true;
-    boolean rightGreenLEDEnabled = true;
-    boolean frontLeftRedLEDEnabled = true;
-    boolean frontLeftGreenLEDEnabled = true;
-    boolean frontRightRedLEDEnabled = true;
-    boolean frontRightGreenLEDEnabled = true;
-    boolean clawServoEnabled = true;
-    boolean coneDistanceSensorEnabled = true;
-    boolean chImuEnabled = true;
-    boolean ehImuEnabled = true;
-    boolean fbImuEnabled = true;
-    boolean parkingCameraEnabled = true;
-    boolean frontCameraEnabled = true;
 
     public RobotHardwareMap(HardwareMap baseHMap, LinearOpMode opmode) {
 
@@ -115,10 +78,9 @@ public class RobotHardwareMap {
         opMode.telemetry.addData("Status", "detecting...");
 
         controlHubBatteryVoltage = baseHMap.get(VoltageSensor.class, "Control Hub");
-        expansionHubBatteryVoltage = baseHMap.get(VoltageSensor.class, "Expansion Hub 2");
+        //expansionHubBatteryVoltage = baseHMap.get(VoltageSensor.class, "Expansion Hub 2");
         controlHub = baseHMap.get(LynxModule.class, "Control Hub");
-        expansionHub = baseHMap.get(LynxModule.class, "Expansion Hub 2");
-
+        //expansionHub = baseHMap.get(LynxModule.class, "Expansion Hub 2");
 
         //dc motor vs dc motor ex?
         backLeftMotor = baseHMap.get(DcMotorEx.class, "RL");
@@ -126,83 +88,21 @@ public class RobotHardwareMap {
         frontLeftMotor = baseHMap.get(DcMotorEx.class, "FL");
         frontRightMotor = baseHMap.get(DcMotorEx.class, "FR");
 
-        //leftTowerMotor = baseHMap.get(DcMotor.class, "TurntableMotorLeft");
-        // rightTowerMotor = baseHMap.get(DcMotor.class, "TurntableMotorRight");
         // intakeMotor = baseHMap.get(DcMotor.class, "IntakeMotor");
 
+        //try {
+        //    leftGreenLED = baseHMap.get(DigitalChannel.class, "leftgreen");
+        //} catch (IllegalArgumentException iae){
+        //    opMode.telemetry.addData("leftgreen", iae.getMessage());
+        //}
 
-        if (elevatorMotorEnabled) {
-            try {
-                elevatorMotor = baseHMap.get(DcMotorEx.class, "elevatorMotor");
-            } catch (IllegalArgumentException iae) {
-                opMode.telemetry.addData("elevatorMotor", iae.getMessage());
-            }
-        }
+        //try {
+        //    leftRedLED = baseHMap.get(DigitalChannel.class, "leftred");
+        //} catch (IllegalArgumentException iae){
+        //    opMode.telemetry.addData("leftred", iae.getMessage());
+        //}
 
-        if (fourBarMotorEnabled) {
-            try {
-                fourBarMotor = baseHMap.get(DcMotorEx.class, "fourBarMotor");
-            } catch (IllegalArgumentException iae) {
-                opMode.telemetry.addData("fourBarMotor", iae.getMessage());
-            }
-        }
-
-        if (clawServoEnabled) {
-            try {
-                clawServo = baseHMap.get(Servo.class, "clawServo");
-            } catch (IllegalArgumentException iae) {
-                opMode.telemetry.addData("clawServo", iae.getMessage());
-            }
-        }
-
-        if (fourBarLowerLimitSwitchEnabled) {
-            try {
-                fourBarLowerLimitSwitch = baseHMap.get(TouchSensor.class, "fLimit");
-            } catch (IllegalArgumentException iae) {
-                opMode.telemetry.addData("fourBarLowerLimitSwitch", iae.getMessage());
-            }
-        }
-
-        try {
-            elevatorMotorLowerLimitSwitch = baseHMap.get(TouchSensor.class, "eLimitDown");
-        } catch (IllegalArgumentException iae){
-            opMode.telemetry.addData("elevatorMotorLowerLimitSwitch", iae.getMessage());
-        }
-
-        try {
-            elevatorMotorUpperLimitSwitch = baseHMap.get(TouchSensor.class, "eLimitUp");
-        } catch (IllegalArgumentException iae){
-            opMode.telemetry.addData("elevatorMotorUpperLimitSwitch", iae.getMessage());
-        }
-
-        if (fourBarPotEnabled) {
-            try {
-                fourBarPot = baseHMap.get(AnalogInput.class, "fourBarPot");
-            } catch (IllegalArgumentException iae) {
-                opMode.telemetry.addData("fourBarPot", iae.getMessage());
-            }
-        }
-
-        if (elevatorColorSensorEnabled) {
-            try {
-                elevatorColorSensor = baseHMap.get(ColorSensor.class, "ecolor");
-            } catch (IllegalArgumentException iae) {
-                opMode.telemetry.addData("elevatorColorSensor", iae.getMessage());
-            }
-        }
-
-        try {
-            leftGreenLED = baseHMap.get(DigitalChannel.class, "leftgreen");
-        } catch (IllegalArgumentException iae){
-            opMode.telemetry.addData("leftgreen", iae.getMessage());
-        }
-
-        try {
-            leftRedLED = baseHMap.get(DigitalChannel.class, "leftred");
-        } catch (IllegalArgumentException iae){
-            opMode.telemetry.addData("leftred", iae.getMessage());
-        }
-
+        /*
         try {
             rightGreenLED = baseHMap.get(DigitalChannel.class, "rightgreen");
         } catch (IllegalArgumentException iae){
@@ -223,19 +123,12 @@ public class RobotHardwareMap {
         } catch (IllegalArgumentException iae){
             opMode.telemetry.addData("lights", iae.getMessage());
         }
-
-        try {
-            coneDistanceSensor = baseHMap.get(DistanceSensor.class, "coneDistance");
-        } catch (IllegalArgumentException iae){
-            opMode.telemetry.addData("coneDistance", iae.getMessage());
-        }
-
-        try {
-            parkingCamera = baseHMap.get(WebcamName.class, "Parking camera");
-            opMode.telemetry.addData("parkingCamera", "success ");
-        } catch (IllegalArgumentException iae){
-            opMode.telemetry.addData("parkingCamera", iae.getMessage());
-        }
+*/
+        //try {
+        //    coneDistanceSensor = baseHMap.get(DistanceSensor.class, "coneDistance");
+        //} catch (IllegalArgumentException iae){
+        //    opMode.telemetry.addData("coneDistance", iae.getMessage());
+        //}
 
         try {
             frontCamera = baseHMap.get(WebcamName.class, "Front camera");
@@ -250,6 +143,7 @@ public class RobotHardwareMap {
         */
 
         //Initializes the IMU
+        /*
         if (chImuEnabled) {
             try {
                 chImu = baseHMap.get(BNO055IMU.class, "chImu");
@@ -273,13 +167,10 @@ public class RobotHardwareMap {
                 opMode.telemetry.addData("fbImu", iae.getMessage());
             }
         }
-
+        */
 
         opMode.telemetry.addData("Status", "done");
         opMode.telemetry.update();
-
-
-
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -1,11 +1,137 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import android.util.Log;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.hardware.RobotControlMechanum;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.TempUnit;
+
+
+import java.io.IOException;
+
+
+@TeleOp
 public class TeleOpMain extends LinearOpMode {
+
+    static final String TAG = "TeleOp Main";
+
     @Override
     public void runOpMode() throws InterruptedException {
+        //Initialize hardware
+        RobotHardwareMap theHardwareMap = new RobotHardwareMap(this.hardwareMap, this);
+        theHardwareMap.initialize();
+
+        RobotControlMechanum robotDrive = new RobotControlMechanum(theHardwareMap, this);
+        robotDrive.initialize();
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+        telemetry.addData("Robot", "Initialized successfully");
+        telemetry.update();
+
+        // waitForStart();
+
+        // do something in init mode?
+        while (opModeInInit()) {
+            telemetry.addData("Robot", "Initialized successfully. Ready to run?");
+            telemetry.update();
+        }
+
+        telemetry.addData("Robot", "running teleop.. press (Y) For telemetry");
+        telemetry.update();
+
+        //Main Loop
+        //set lights for slow mode
+        double loopTimeStart = 0;
+        boolean slowMode = true;
+        boolean showTelemetry = false;
+
+        //create some gamepads to look at debouncing
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad currentGamepad2 = new Gamepad();
+        Gamepad previousGamepad1 = new Gamepad();
+        Gamepad previousGamepad2 = new Gamepad();
+        ElapsedTime lastFourBarMove = new ElapsedTime();
+        ElapsedTime lastElevatorMove = new ElapsedTime();
+
+        while (opModeIsActive()) {
+
+            loopTimeStart = System.currentTimeMillis();
+
+            //copy over the previous gamepads so we can compare what changed
+            previousGamepad1.copy(currentGamepad1);
+            previousGamepad2.copy(currentGamepad2);
+
+            currentGamepad1.copy(gamepad1);
+            currentGamepad2.copy(gamepad2);
+
+            //mechanum drive - left stick y is negative because the up is negative
+            double drive = -1 * gamepad1.left_stick_y;
+            double strafe = -1 * gamepad1.left_trigger + gamepad1.right_trigger;
+            //double strafe = gamepad1.left_stick_x;
+            double twist = gamepad1.right_stick_x;
+
+            if (slowMode) {
+                drive *= 0.5;
+                strafe *= 0.5;
+                twist *= 0.5;
+
+            } else { // non slow mode is only 75% power
+                drive *= 0.75;
+                strafe *= 0.75;
+                twist *= 0.75;
+            }
+
+            robotDrive.teleOpMechanum(drive, strafe, twist);
+
+            //slow mode
+            if (currentGamepad1.left_bumper && !previousGamepad1.left_bumper) {
+                slowMode = true;
+                //lights.switchLight(Light.ALL_LEFT, LightMode.YELLOW);
+            } else if (currentGamepad1.right_bumper && !previousGamepad1.right_bumper) {
+                slowMode = false;
+                //lights.switchLight(Light.ALL_LEFT, LightMode.GREEN);
+            }
+
+            //show telemetry
+            if (currentGamepad1.y && !previousGamepad1.y) {
+                showTelemetry = !showTelemetry;
+            }
+
+            if (showTelemetry) {
+                telemetry.addData("Control1 Left X",currentGamepad1.left_stick_x);
+                telemetry.addData("Control1 Left Y",currentGamepad1.left_stick_y);
+                telemetry.addData("Control1 Right X",currentGamepad1.right_stick_x);
+                telemetry.addData("Control Right Y",currentGamepad1.right_stick_y);
+                telemetry.addData("Bumper Left",currentGamepad1.left_bumper);
+                telemetry.addData("Bumper Right",currentGamepad1.right_bumper);
+                //telemetry.addData("ch (mA)", theHardwareMap.controlHub.getCurrent(CurrentUnit.AMPS));
+                //telemetry.addData("eh (mA)", theHardwareMap.expansionHub.getCurrent(CurrentUnit.AMPS));
+                //telemetry.addData("ch temp", theHardwareMap.controlHub.getTemperature(TempUnit.FARENHEIT));
+                //telemetry.addData("eh temp", theHardwareMap.expansionHub.getTemperature(TempUnit.FARENHEIT));
+            }
+
+            telemetry.addData("looptime", System.currentTimeMillis() - loopTimeStart);
+            telemetry.update();
+
+
+        }
+
+        telemetry.addData("Status", "Stopped");
+        telemetry.update();
+
     }
 }
