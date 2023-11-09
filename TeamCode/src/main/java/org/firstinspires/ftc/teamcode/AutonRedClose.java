1d6f220b02c19eb57fcd39b6fce4b60b14d09b49
reslip <reslip@icloud.com>
2023-11-08 20:55:49 -0500

    merged autonredclose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedClose.java
@@ -2,24 +2,64 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.hardware.ArmPositions;
 import org.firstinspires.ftc.teamcode.hardware.GripperPositions;
-import org.firstinspires.ftc.teamcode.hardware.RobotControlArm;
-import org.firstinspires.ftc.teamcode.hardware.RobotControlGripperServos;
+import org.firstinspires.ftc.teamcode.hardware.Light;
+import org.firstinspires.ftc.teamcode.hardware.LightMode;
 
 @Autonomous(name = "Auton Red Close To Backstage", group = "Autons")
 public class AutonRedClose extends AutonBase {
 
-    SpikeLocationDetectionPipeline spikeLocation;
+
+
+    private int     parkingPosition= -1;
+    private int     lastParkingPosition = -1;
     @Override
     public void runOpMode(){
-        spikeLocation = new SpikeLocationDetectionPipeline(telemetry);
+
+
+
+
+        boolean left = false;
+        boolean middle = false;
+        boolean right = true;
 
         initialize();
+
+        while (opModeInInit()) {
+            //check that the camera is open and working
+            if (robotCameraHandler.frontCameraIsFaulty.get() && robotCameraHandler.enableFrontCamera)
+                telemetry.addData("Status", "Front camera fault...Please restart");
+            else
+                telemetry.addData("Status", "Front Camera initialized");
+
+            try {
+                parkingPosition = robotCameraHandler.spikeLocationDetectionPipeline.getSpikeLocation().get();
+                telemetry.addData("Parking Position", parkingPosition);
+            } catch (NullPointerException npe) {
+                telemetry.addData("cameras", npe.getMessage());
+            }
+            telemetry.update();
+
+            if (lastParkingPosition != parkingPosition) {
+                if (parkingPosition == 1) {
+
+                    lights.switchLight(Light.ALL_LEFT, LightMode.RED);
+                } else if (parkingPosition == 2) {
+                    lights.switchLight(Light.ALL_LEFT, LightMode.YELLOW);
+                } else if (parkingPosition == 3) {
+                    lights.switchLight(Light.ALL_LEFT, LightMode.GREEN);
+                } else if (parkingPosition == 0) {
+                    lights.switchLight(Light.ALL_LEFT, LightMode.OFF);
+                }
+            }
+        }
+
         waitForStart();
 
         //Left
-        if(false){
+        if(parkingPosition == 1){
             imuDrive(.4, 29.5, 0);
             imuTurn(.3, -90);
             imuDrive(.25, 7, 0);
@@ -38,7 +78,7 @@ public class AutonRedClose extends AutonBase {
             imuDrive(0.5, 15, 0);
         }
         //Middle
-        else if(true){
+        else if(parkingPosition == 2){
             // lift arm before you start driving
             imuDrive(.3, 36, 0);
             sleep(750);
@@ -52,7 +92,7 @@ public class AutonRedClose extends AutonBase {
             imuDrive(0.5, -13, 0);
         }
         //Right
-        else if(false){
+        else if(parkingPosition == 3){
             imuDrive(.4, 19.5, 0);
             imuTurn(0.4, 61);
             imuDrive(0.25, 7, 0);
