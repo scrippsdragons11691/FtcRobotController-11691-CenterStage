e8a15608bbe3c0d46b990a063a58ae6a7a786fa1
khoslamanav25 <92478501+khoslamanav25@users.noreply.github.com>
2023-11-08 20:48:59 -0500

    Auton Red Close

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedClose.java
@@ -11,41 +11,38 @@ import org.firstinspires.ftc.teamcode.hardware.RobotControlGripperServos;
 public class AutonRedClose extends AutonBase {
 
     SpikeLocationDetectionPipeline spikeLocation;
-    RobotControlGripperServos clawServo1;
-    RobotControlGripperServos clawServo2;
-    RobotControlArm armMotor;
     @Override
     public void runOpMode(){
         spikeLocation = new SpikeLocationDetectionPipeline(telemetry);
-
-        clawServo1 = new RobotControlGripperServos(theHardwareMap, this, "ServoClaw1");
-        clawServo2 = new RobotControlGripperServos(theHardwareMap, this, "ServoClaw2");
-        armMotor = new RobotControlArm(theHardwareMap,this);
-
-        clawServo1.initialize();
-        clawServo2.initialize();
-        armMotor.initialize();
-
-        boolean left = false;
-        boolean middle = false;
-        boolean right = true;
 
         initialize();
         waitForStart();
 
         //Left
-        if(spikeLocation.getSpikeLocation().get() == 1){
-
+        if(false){
+            imuDrive(.4, 29.5, 0);
+            imuTurn(.3, -90);
+            imuDrive(.25, 7, 0);
+            imuDrive(.15, -3, 0);
+            sleep(1000);
+            imuDrive(.4, -20, 0);
+            imuTurn(.25, -15);
+            imuDrive(.25, -14.5, 0);
+            imuTurn(.25, 15);
+            imuDrive(.25, -15, 0);
+            sleep(5000);
+            imuDrive(.25, 5, 0);
+            imuTurn(0.75, 90);
+            imuDrive(.5, 16, 0);
+            imuTurn(0.75, 90);
+            imuDrive(0.5, 15, 0);
         }
         //Middle
-        else if(spikeLocation.getSpikeLocation().get() == 2){
+        else if(true){
             // lift arm before you start driving
             imuDrive(.3, 36, 0);
             sleep(750);
             imuDrive(.15, -5, 0);
-            armMotor.moveArmEncoded(ArmPositions.FRONT_ARC_MIN);
-            clawServo1.moveToPosition(GripperPositions.GRIPPER1_OPEN);
-            clawServo2.moveToPosition(GripperPositions.GRIPPER2_OPEN);
             sleep(250);
             imuDrive(.15, -4, 0);
             imuTurn(.75, -90);
@@ -55,23 +52,24 @@ public class AutonRedClose extends AutonBase {
             imuDrive(0.5, -13, 0);
         }
         //Right
-        else if(spikeLocation.getSpikeLocation().get() == 3){
-            encoderStrafe(.4, 5, 0);
-            imuDrive(.3, 24, 0);
+        else if(false){
+            imuDrive(.4, 19.5, 0);
+            imuTurn(0.4, 61);
+            imuDrive(0.25, 7, 0);
             sleep(750);
-            imuDrive(.3, -8, 0);
-            imuTurn(.75, -90);
-            imuDrive(0.5, -30, 0);
+            imuDrive(0.25, -5.5, 0);
+            imuTurn(.5, -150);
+            imuDrive(0.5, -37.5, 0);
             //replace delay by placing pixel on backdrop
             sleep(5000);
-            encoderStrafe(0.5, -28, 5);
-            imuDrive(0.5, 13, 0);
+            encoderStrafe(0.5, 35                   , 5);
+            imuDrive(0.5, -13, 0);
         }
         //Error
         else{
-            imuDrive(.3, 36, 0);
-            sleep(500);
-            imuDrive(.15, -5, 0);
+            imuDrive(.5, 36, 0);
+            imuDrive(.15, 5, 0);
+            imuDrive(.25, 5, 0);
             sleep(2000);
             imuDrive(.15, -4, 0);
             imuTurn(.75, -90);
