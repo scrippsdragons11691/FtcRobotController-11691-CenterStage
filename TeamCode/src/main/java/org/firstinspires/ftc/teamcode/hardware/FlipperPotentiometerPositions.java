c4b8a3a2ba8da65e0ac77e56a90e18229da96517
Jason P <greg_prater@yahoo.com>
2024-03-19 19:22:59 -0400

    Updates to servo claw values and flipper pot values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/FlipperPotentiometerPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/FlipperPotentiometerPositions.java
@@ -3,10 +3,10 @@ package org.firstinspires.ftc.teamcode.hardware;
 public enum FlipperPotentiometerPositions {
 
     MIN_VOLTAGE("MIN_VOLTAGE", 0),
-    CLAW1_DOWN("CLAW1_DOWN", 2.892),
-    CLAW2_DOWN("CLAW2_DOWN", 0.455),
-    CLAW1_PLACE("CLAW1_PLACE", 0),
-    CLAW2_PLACE("CLAW2_PLACE", 1.858),
+    CLAW1_DOWN("CLAW1_DOWN", .2),
+    CLAW2_DOWN("CLAW2_DOWN", 1.455),
+    CLAW1_PLACE("CLAW1_PLACE", 1.6),
+    CLAW2_PLACE("CLAW2_PLACE", 0),
     MAX_VOLTAGE("MAX_VOLTAGE", 4);
 
     private final String position;
